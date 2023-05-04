99df524d0cfe67fa8393aa1e84356855b293a176
梨花不等故人来 <109089458+timeless157@users.noreply.github.com>
2023-05-04 15:56:23 +0800

    尝试ES，依赖没成功

--- timeless-blog/src/main/java/com/timeless/chatGpt/ChatGptUtils.java
+++ timeless-blog/src/main/java/com/timeless/chatGpt/ChatGptUtils.java
@@ -37,7 +37,7 @@ public class ChatGptUtils {
                 .url("https://api.openai.com/v1/chat/completions")
                 .post(body)
                 .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", "Bearer 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") // Replace with your actual API key
+                .addHeader("Authorization", "Bearer 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") // Replace with your actual API key
                 .build();
         Response response = client.newCall(request).execute();
         String responseData = response.body().string();
